d37eba95cfc317e79f0774ca13e7082d0b02042b
Wiktor Malinowski <maliniak@MacBook-Air-Wiktor.local>
2022-03-24 21:19:33 +0100

    Stage 3 implemented (/return endpoint and randomized tokens (UUID) added)

--- Cinema Room REST Service/task/src/cinema/Seat.java
+++ Cinema Room REST Service/task/src/cinema/Seat.java
@@ -1,52 +1,69 @@
 package cinema;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Seat {
     private int row;
     private int column;
     private int price;
     @JsonIgnore
-    private boolean isAvaliable;
+    private boolean isAvailable;
+    @JsonIgnore
+    private Token token;
 
-    public Seat() {
-        super();
-    }
 
-    public Seat(int row, int column){
+
+    @JsonCreator
+    public Seat(@JsonProperty("row") int row, @JsonProperty("column") int column){
         this.row = row;
         this.column = column;
-        setPrice(column);
-        this.isAvaliable = true;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
+        setPrice(row);
+        this.isAvailable = true;
+        this.token = new Token();
     }
 
     public int getRow() {
         return row;
     }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
+
     public int getColumn() {
         return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
     }
 
     public int getPrice() {
         return price;
     }
+
     private void setPrice(int row) {
         this.price = row > 4 ? 8 : 10;
     }
-    public void setIsAvaliable(boolean isAvaliavle) {
-        this.isAvaliable = isAvaliavle;
+
+    public boolean getIsAvailable() {
+        return isAvailable;
     }
-    public boolean getIsAvaliable() {
-        return isAvaliable;
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
     }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+
+
 }
